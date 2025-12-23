f681f0f9b28af859fddb589ffa739adeb74aa89e
Delalel <lurdasha03@mail.ru>
2025-12-23 18:31:48 +0700

    Update AuthorizationWindow.xaml.cs

--- Konstr/AuthorizationWindow.xaml.cs
+++ Konstr/AuthorizationWindow.xaml.cs
@@ -14,9 +14,6 @@ using System.Windows.Shapes;
 
 namespace Konstr
 {
-    /// <summary>
-    /// Логика взаимодействия для AuthorizationWindow.xaml
-    /// </summary>
     public partial class AuthorizationWindow : Window
     {
         public AuthorizationWindow()
